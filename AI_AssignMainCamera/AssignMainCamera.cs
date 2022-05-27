c9573c2c953d338b0f91dfd26e1545efefa21892
KKY <8879012+kkykkykky@users.noreply.github.com>
2022-05-27 12:15:12 -0600

    Initial commit

---
+++ AI_AssignMainCamera/AssignMainCamera.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Studio;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AssignMainCamera
+{
+    [BepInPlugin(GUID, PLUGINNAME, VERSION)]
+    [BepInProcess("StudioNEOV2.exe")]
+    public class AssignMainCamera : BaseUnityPlugin
+    {
+        public const string GUID = "kky.ai.assignmaincamera";
+        public const string PLUGINNAME = "AI Assign Main Camera";
+        public const string VERSION = "1.0.1";
+
+        private void Awake()
+        {
+            var harmony = new Harmony(nameof(AssignMainCamera));
+            harmony.PatchAll(typeof(AssignMainCamera));
+        }
+
+        [HarmonyPatch(typeof(Studio.CameraControl), "Awake")]
+        [HarmonyPostfix]
+        public static void setMainCamera(Studio.CameraControl __instance)
+        {
+            if (__instance.mainCmaera == null) __instance.mainCmaera = Camera.main;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Assign Main Camera Plugin
+
+Sets mainCamera in Studio.CameraControl to Camera.main for AI Shoujo. This fixes the issue with the Timeline plugin when it tries to get mainCamera.
+
+**Downlaod:**
+ - Get latest version from the Releases page.
+
+**Installation:**
+ - Extract to your game folder or put the .dll file in BepInEx\Plugins.
